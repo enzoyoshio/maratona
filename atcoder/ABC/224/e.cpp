ad51b376569a3d7befacc5f018cbdd5b176e1d60
Enzo Yoshio <enzoyn@hotmail.com>
2023-07-03 17:21:00 -0300

    no linux

---
+++ atcoder/ABC/224/e.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Template (v1.4.2 - 2023-04-22) (codeforces:EYZ, atcoder:EYZ) {{{
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
+#define int long long
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr)
+
+template<class T> using V = vector<T>;
+template<class T> using min_priority_queue = priority_queue<T, vector<T>, greater<T>>;
+using ii = pair<int, int>;
+using iii = array<int, 3>;
+
+#define all(c) c.begin(), c.end()
+#define rall(c) c.rbegin(), c.rend()
+#define sz(x) (int) (x).size()
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define nemo ><>
+#define loop(ii, n) for (int ii = 0; ii < (n); ii++)
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define mem(a, b) memset((a), (b), sizeof(a))
+#define inbounds(x, l, r) ((l) <= (x) && (x) <= (r))
+#define L1(res...) [&](auto x){ return res; }
+#define L2(res...) [&](auto x, auto y){ return res; }
+
+template<class T, class U> inline void miq(T& a, U b){ if (a > b) a = b; }
+template<class T, class U> inline void maq(T& a, U b){ if (a < b) a = b; }
+
+template<class T, class U> auto &operator>>(istream &is, pair<T, U> &p) { return is >> p.ff >> p.ss; }
+template<class T, class U> auto &operator<<(ostream &os, pair<T, U> const& p) { return os << '(' << p.first << ' ' << p.second << ')'; }
+
+const auto EMPTY_STRING = "", SEPARATOR = " ";
+template<class T> auto &operator>>(istream& is, vector<T> &c) { for (auto &x : c) is >> x; return is; }
+template<class T> auto &operator<<(ostream& os, vector<T> const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
+template<class T> auto &operator<<(ostream& os, set<T> const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
+template<class T> auto &operator<<(ostream& os, multiset<T> const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
+template<class T> auto &operator<<(ostream& os, unordered_set<T> const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
+template<class T> auto &operator<<(ostream& os, ordered_set<T> const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
+template<class T> auto &operator<<(ostream& os, deque<T> const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
+template<class K, class V> auto &operator<<(ostream& os, map<K,V> const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
+template<class K, class V> auto &operator<<(ostream& os, unordered_map<K,V> const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
+
+template<class... A> void in(A &...a) { ((cin >> a), ...); }
+template<class... A> void out(A const&... a) { auto sep = EMPTY_STRING; ((cout << sep << a, sep = SEPARATOR), ...); cout << '\n'; }
+template<class... A> void print(A const&... a) { ((cout << a), ...); }
+#define var(x) "[", #x, " ", x, "] "
+template<class... A> void db(A const&... a) { ((cout << (a)), ...); cout << endl; }
+//}}}
+
+using iiii = array<int, 4>;
+
+auto main() -> signed {
+  fastio;
+
+  int h, w, n;
+  in(h, w, n);
+  //map<int, V<iiii>> value;
+  V<iiii> reading;
+  for(int i = 0; i < n; i++) {
+    int r, c, a; in(r, c, a);
+    //value[-a].eb(iiii{a, r, c, i});
+    reading.eb(iiii{a, r, c, i});
+  }
+  sort(rall(reading));
+  V<V<iiii>> value;
+
+  for(auto [a, r, c, i]: reading) {
+    if(value.empty() || value.back()[0][0] != a) {
+      value.pb({iiii{a, r, c, i}});
+    }else value.back().eb(iiii{a, r, c, i});
+  }
+
+  V<int> ans(n);
+  vector<int> xs(h+1), ys(w+1);
+  for(auto cur_value: value) {
+    for(auto [a, r, c, idx]: cur_value) {
+      ans[idx] = max(xs[r]+1, ys[c]+1);
+    }
+
+    for(auto [a, r, c, idx]: cur_value) {
+      xs[r] = max(xs[r], ans[idx]);
+      ys[c] = max(ys[c], ans[idx]); 
+    }
+  }
+
+  for(auto it: ans)
+    cout << it-1 << '\n';
+}
